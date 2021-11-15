c02c151d65e3d15156de5c5bba472b7156a458ce
bielusantos <bielusantos@ucl.br>
2021-11-15 18:30:21 -0300

    Adicionando a primeira operacao para descer

---
+++ operarElevador.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Elevador
+{
+    public class ElevadorDescendo : Estado
+    {
+        private readonly int _andarDesejado;
+
+        public ElevadorDescendo(int andarDesejado, Estado estado)
+            : this(estado.AndarAtual, estado.Controle, estado.Elevador)
+        {
+            _andarDesejado = andarDesejado;
+        }
+
+        private ElevadorDescendo(int andarAtual, bool[] controle, ControleElevador elevador)
+        {
+            AndarAtual = andarAtual;
+            Controle = controle;
+            Elevador = elevador;
+        }
+
+        public override string MovimentarElevador()
+        {
+            var mensagem = string.Empty;
+
+            if (_andarDesejado < 1 || _andarDesejado > Elevador.ObterAndares())
+                return "Andar inválido.";
+
+            for (int i = AndarAtual; i >= 1; i--)
+            {
+                if (Controle[i])
+                {
+                    mensagem = PararNoAndar(_andarDesejado);
+                    Elevador.EstadoAtual = new ElevadorParado(this);
+                    break;
+                }
+                else
+                    continue;
+            }
+
+            return mensagem ?? "Andar não encontrado!";
+        }
+    }
+}
