6a78b893dff27e4f02ba6cb4c76d258b79b59665
bielusantos <bielusantos@ucl.br>
2021-11-15 18:06:35 -0300

    Adicionando controle de estado e renomeando arquivos

---
+++ Estado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Elevador
+{
+    public class Estado
+    {
+        public ControleElevador Elevador { get; set; }
+        public int andarAtual { get; set; }
+        public bool[] Controle { get; set; }
+
+        public abstract string MovimentarElevador();
+
+        public string AndarDesejado(int andar) 
+        {
+            Controle[andarAtual] = false;
+            Controle[andar] = true;
+            andarAtual = andar;
+
+            return ($"Elevador parado no andar {andar} andar");
+        }
+    }
+
+}
