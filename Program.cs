71ec23e3ceaee0c803136339720438155f4ae911
bielusantos <bielusantos@ucl.br>
2021-11-15 17:18:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Elevador
+{
+    public class ControleElevador
+    {
+        public Estado estadoAtual;
+        private int andares;
+        public ControleElevador(int andaresPredio)
+        {
+            andares = andaresPredio;
+            estadoAtual = new ElevadorParado(1, this);
+
+        }
+
+        public void IniciarElevador()
+        {
+            while (true)
+            {
+                Console.WriteLine(estadoAtual.MovimentarElevador());
+            }
+        }
+    }
+}
